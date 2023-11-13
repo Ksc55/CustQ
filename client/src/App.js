7b695fcf96f4dd4e15ef2b4fa2082a30b005e53c
Raja Kumar <rajakumarmahto952@gmail.com>
2023-11-13 22:16:42 +0530

    adding navigation

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import DeleteConfirm from "./pages/DeleteConfirm";
 import ResetConfirm from "./pages/ResetConfirm";
 import TokenCreation from "./pages/TokenCreation";
 import Home from "./pages/Home";
+import TokenSettings from "./pages/TokenSettings";
+import Dashboard from "./pages/Dashboard";
+import Reports from "./pages/Reports";
 
 function App() {
   return (
@@ -21,6 +24,9 @@ function App() {
           <Route path="/deleteconfirm" element={<DeleteConfirm />} />
           <Route path="/resetconfirm" element={<ResetConfirm />} />
           <Route path="/tokencreation" element={<TokenCreation />} />
+          <Route path="/tokensettings" element={<TokenSettings />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/reports" element={<Reports />} />
           <Route path="*" element={<Error404 />} />
           <Route path="/error404" element={<Error404 />} />
         </Routes>
